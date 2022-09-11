fe3e674415c5a3d932952bea8c4b6ae981303412
Mateus Santos <a.mateussm1.5@gmail.com>
2022-09-11 15:14:39 -0300

    :wrench: | Event close

--- calax/model/service/calax/events/__init__.py
+++ calax/model/service/calax/events/__init__.py
@@ -5,25 +5,43 @@ from discord.message import Message
 
 @calax.bot.event
 async def on_ready():
-    # Adicionar todas as pessoas nas salas de voz no objeto sala
     id_auth_message: str = calax.id_auth_message
     id_auth_channel: str = calax.id_auth_channel
-    print(f'id_auth_message: {id_auth_message}')
-    print(f'id_auth_channel: {id_auth_channel}')
-    channel: TextChannel = calax.bot.get_channel(int(id_auth_channel))
-    message: Message = await channel.fetch_message(int(id_auth_message))
-    await message.clear_reaction("👍")
-    await message.add_reaction("👍")
-    for room in calax.rooms:
-        voice_channel = calax.bot.get_channel(int(room.id_voice_channel))
-        voice_channel_members = voice_channel.members
+    try:
+        channel: TextChannel = calax.bot.get_channel(int(id_auth_channel))
+        message: Message = await channel.fetch_message(int(id_auth_message))
+        # Clear all reaction in auth message
+        await message.clear_reaction("👍")
+        # Add first reaction in auth message
+        await message.add_reaction("👍")
+        for room in calax.rooms:
+            voice_channel = calax.bot.get_channel(int(room.id_voice_channel))
+            voice_channel_members = voice_channel.members
 
-        # Add player that is in game voice channel into its room
-        ids_in_room: list[int] = [player.id for player in room.players]
-        for voice_channel_member in voice_channel_members:
-            # List all rooms to see if this player is in another one
-            if str(voice_channel_member.id) in ids_in_room:
-                room.addPlayer(
-                    player = Player(str(voice_channel_member.id))
-                )
-    
+            # Add player that is in game voice channel into its room
+            ids_in_room: list[int] = [player.id for player in room.players]
+            for voice_channel_member in voice_channel_members:
+                # List all rooms to see if this player is in another one
+                if str(voice_channel_member.id) in ids_in_room:
+                    room.addPlayer(
+                        player = Player(str(voice_channel_member.id))
+                    )
+    except Exception as exception:
+        if exception.code == 10008:
+            print('Auth-message not found')
+
+@calax.bot.event
+async def close():
+    id_auth_message: str = calax.id_auth_message
+    id_auth_channel: str = calax.id_auth_channel
+    try:
+        channel: TextChannel = calax.bot.get_channel(int(id_auth_channel))
+        message: Message = await channel.fetch_message(int(id_auth_message))
+        # Clear all reaction in auth message
+        await message.clear_reaction("👍")
+        # Add first reaction in auth message
+        await message.add_reaction("👍")
+    except Exception as exception:
+        if exception.code == 10008:
+            print('Auth-message not found')
+

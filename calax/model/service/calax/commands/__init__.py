3173cc7ba4608907a12e2279185609a46eb4ebd8
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-10-08 18:03:07 -0300

    :tada: | Command: op -> Choose an option

--- calax/model/service/calax/commands/__init__.py
+++ calax/model/service/calax/commands/__init__.py
@@ -94,7 +94,7 @@ async def girar(context: Context):
             # the victim
             while not room.game.is_victim_a_asker:
                 room.game.victim = choice(room.game.players)
-                if room.game.victim.id != room.game.asker:
+                if room.game.victim.id != room.game.asker.id:
                     room.game.is_victim_a_asker = True
 
             # Show the bottle spining
@@ -118,3 +118,41 @@ async def girar(context: Context):
         else:
             ...
             # await context.send(f'Não é possível girar a garrafa agora agora.')
+
+# Command to choose if it's verdade or consequencia
+@calax.bot.command()
+async def op(
+    context: Context,
+    option: str = ''
+):
+    option = option.lower()
+    player: Player = Player(str(context.author.id))
+    player.user = calax.bot.get_user(int(player.id))
+    player_room: Room = findRoomInCalaxByPlayerId(player.id, calax)
+    for room in calax.rooms:
+        if str(context.channel.id) == room.id_txt_channel and\
+        room.game.fase_controller == 2 and\
+        room.game.victim.id == player.id:
+            if option == 'v':
+                # It can choose truth
+                if room.game.victim.number_of_truths <= 3:
+                    room.game.victim.response = 'verdade'
+                    await context.send(f'<@{room.game.asker.id}>, faça sua pergunta.')
+                    room.game.victim.number_of_truths += 1
+                    room.game.fase_controller = 3
+                    break
+                # It must choose challenge
+                else:
+                    option = 'c'
+                    await context.send(
+                        f'<@{room.game.victim.id}> você escolheu 3 vezes verdade. Agora será feito um desafio para você.'
+                    )
+                    room.game.victim.number_of_truths = 0
+            if option == 'c':
+                room.game.victim.response = 'consequencia'
+                await context.send(f'<@{room.game.asker.id}>, faça seu desafio.')
+                room.game.fase_controller = 3
+                break
+        else:
+            ...
+            # await context.send(f'Não é possível enviar uma resposta para o bot agora.')

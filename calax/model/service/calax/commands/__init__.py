7ade41ddb843cc1dba116da54de3ea9278ce974c
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-10-09 16:44:52 -0300

    :tada: | Command: restart the game

--- calax/model/service/calax/commands/__init__.py
+++ calax/model/service/calax/commands/__init__.py
@@ -255,3 +255,31 @@ async def feito(context: Context):
         else:
             ...
             # await context.send(f'<@{player.id}>, você não pode responder agora.')
+
+# Restart the game
+@calax.bot.command()
+async def restart(context: Context):
+    await context.message.delete()
+    player: Player = Player(str(context.author.id))
+    player.user = calax.bot.get_user(int(player.id))
+    player_room: Room = findRoomInCalaxByPlayerId(player.id, calax)
+    for room in calax.rooms:
+        if str(context.channel.id) == room.id_txt_channel and\
+        room.game.bot_master.id == player.id:
+            room.game.fase_controller = 0
+            room.game.players_pointer = 0
+            room.game.asker = None
+            room.game.victim = None
+            message = await context.send("Reloading")
+            clock_emojis: list[str] = [
+                "🕛", "🕜", "🕑"
+            ]
+            for index, clock_emoji in enumerate(clock_emojis):
+                await message.edit(
+                    content = f'{clock_emoji} | Reloading' + '. ' * (index + 1)
+                )
+                sleep(.2)
+            await message.edit(content="✅ | Done!")
+            sleep(.2)
+            await message.delete()
+            break
